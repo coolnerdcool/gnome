5fe0e7e4081099cd4cf53c6f00d6c022436f19ec
Michel Ortega <coolnerdcool@gmail.com>
2019-10-21 13:49:06 -0500

    Initial Commit

---
+++ Project7_100/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project7_100
+//
+//  Created by Michel Ortega on 10/21/19.
+//  Copyright © 2019 Michel Ortega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
