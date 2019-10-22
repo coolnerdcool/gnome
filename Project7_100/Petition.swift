658b18c2002750c635e9b609a2af64e770ae196b
Michel Ortega <coolnerdcool@gmail.com>
2019-10-21 19:31:23 -0500

    [FEAT]: Add presenter, a mock model  and UITableViews to the storyboard.

---
+++ Project7_100/Petition.swift
@@ -0,0 +1,16 @@
+//
+//  Petition.swift
+//  Project7_100
+//
+//  Created by Michel Ortega on 10/21/19.
+//  Copyright © 2019 Michel Ortega. All rights reserved.
+//
+
+import Foundation
+
+struct Petition: Codable {
+	var title: String
+	var body: String
+	var signatureCount: Int
+	
+}
---
+++ Project7_100/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7_100
+//
+//  Created by Michel Ortega on 10/21/19.
+//  Copyright © 2019 Michel Ortega. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+	var results = [Petition]()
+}
